c5c76bf4a92e7965da0db168b3af70c87a779e89
Dirk Krummacker <rasen_02cider@icloud.com>
2023-02-25 17:55:00 +0100

    Fix failing unit test

--- Tests/levenshteinTests/levenshteinTests.swift
+++ Tests/levenshteinTests/levenshteinTests.swift
@@ -3,9 +3,17 @@ import XCTest
 
 final class levenshteinTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(levenshtein().text, "Hello, World!")
+        let pairs: [[String]] = [
+            ["Kaserne", "Kaverne", "1"],
+            ["Himmel", "Hölle", "3"],
+            ["Grün", "Blau", "4"],
+            ["Hut", "Donaudampfschiffahrtskapitänsmütze", "3"],
+            ["Zitrone", "Dampfschiff", "7"]
+        ]
+        for pair in pairs{
+            let actual = String(levenshteinDistance(s1: pair[0], s2: pair[1]))
+            let expected = pair[2]
+            XCTAssertEqual(actual, expected)
+        }
     }
 }
